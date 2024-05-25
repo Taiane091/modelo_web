37e449a547040c61ac4f93e9d2b1d236ed6f7fe0
Taiane091 <127451386+Taiane091@users.noreply.github.com>
2024-05-25 00:11:44 +0000

    24/05/2024

--- modelo_web/resources/views/livros.blade.php
+++ modelo_web/resources/views/livros.blade.php
@@ -4,26 +4,36 @@
 <HTML>
     <HEAD>
         <TITLE>{{$TITLE ?? 'Meu site'}}</TITLE>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="{{ asset('/css/app.css')}}">
 </HEAD>
     <body>
         <x-layout>
 </x-layout>
 <h1 id="titulo">DESCRIÇÃO DE LIVROS</h1>
-
+<x-carrosel 
+    img1="https://cdn.pensador.com/img/imagens/oe/st/o_estrangeiro.jpg?auto_optimize=low&width=655"
+    img2="https://cdn.pensador.com/img/imagens/pe/ns/pensador_extraordinario.jpg?auto_optimize=low&width=655"
+    img3="https://cdn.pensador.com/img/imagens/am/et/a_metamorfose.jpg?auto_optimize=low&width=655"
+    img4="https://cdn.pensador.com/img/imagens/vi/da/vidas_secas_1_c.jpg?auto_optimize=low&width=655"
+    img5="https://cdn.pensador.com/img/imagens/so/br/sobre_a_terra.jpg?auto_optimize=low&width=655"
+/>
 <h1>O Estrangeiro</h1>
-<img src="https://cdn.pensador.com/img/imagens/oe/st/o_estrangeiro.jpg?auto_optimize=low&width=655" alt="">
+
 <p>O Estrangeiro” faz parte do chamado Ciclo dos Absurdos, do romancista francês Albert Camus, composto pelo romance “O Estrangeiro”, 
     o ensaio “O Mito de Sísifo” e a peça teatral “Calígula”.
      Esta obra de “alma existencialista” pode ser apreciada em apenas algumas horas!!!</p>
      <h1>A Metamorfose</h1>
-     <img src="https://cdn.pensador.com/img/imagens/am/et/a_metamorfose.jpg?auto_optimize=low&width=655" alt="">
+
      <p>Esta é uma das obras mais conhecidas de Kafka e, com certeza, uma das mais perturbadoras da literatura moderna! “A Metamorfose” é uma pequena novela que narra a história de um caixeiro viajante que acorda metamorfoseado em um inseto repugnante!
         Percursor da chamada “ficção absurda”, Kafka levou menos de um mês para escrever “A Metamorfose”, e você levará menos de um dia para apreciar esta leitura.</p>
 <h1>Extraordinário</h1>  
-<img src="https://cdn.pensador.com/img/imagens/pe/ns/pensador_extraordinario.jpg?auto_optimize=low&width=655" alt="">   
 <p>A emocionante história de Auggie não deixará você largar este livro até terminar de ler a última linha! “Extraordinário” é um sucesso da literatura infanto juvenil contemporânea, apresentando como plano de fundo o modo como as pessoas lidam com as diferenças e o preconceito. Imperdível! O livro foi adaptado para o cinema em 2017 com Jacob Tremblay (O Quarto de Jack) como Auggie.</p>
-
+<h1>Vidas Secas</h1>
+<p>Um dos maiores clássicos da literatura brasileira, “Vidas Secas”, do escritor Graciliano Ramos, relata as dificuldades sofridas por uma família de retirantes no sertão do Brasil no começo do século XX.
+<p>
+Ao longo da história dividida em 13 capítulos, acompanhamos pequenas passagens da vida de Fabiano, Sinhá Vitória e seus dois filhos. Outro personagem marcante neste livro é a cadela Baleia, adorada pelos filhos da família, mas que acaba adoecendo ao longo da história. A adaptação da novela em filme, dirigida por Nelson Pereira dos Santos, recebeu o prêmio OCIC no Festival de Cannes de 1964.</p>
 <footer>
 </hr>
 DESCRIÇÃO DE LIVROS
